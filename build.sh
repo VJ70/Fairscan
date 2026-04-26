7eda62f3960b567c6600861ba6804bab2cb33ca0
VJ70 <vaishnavi7a7@gmail.com>
2026-04-27 00:14:58 +0530

    fix: correct flutter sdk download url typo

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -fsSL https://storage.googleapis.com/flutter-infra-release/releases/stable/linux/flutter_linux_3.19.0-stable.tar.xz | tar xJ
+curl -fsSL https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.22.0-stable.tar.xz | tar xJ
 export PATH="$PATH:$(pwd)/flutter/bin"
 cd frontend
 flutter pub get
